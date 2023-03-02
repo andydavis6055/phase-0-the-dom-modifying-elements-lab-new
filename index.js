25b3a1ec42a59a6ca7c58013dee078fc2bff3d18
Andy Davis <andydavis6055@gmail.com>
2023-03-02 14:20:19 -0700

    all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove();
+const newHeader = document.createElement("h1")
+document.getElementById('victory')
+newHeader.setAttribute('id', 'victory')
+newHeader.innerHTML=
+    "Andy is the champion"
